e98b05aafe50e38f7135e58963510384940e4b1c
Marius Steinbach <marius@steinbach.io>
2017-12-23 16:44:23 +0100

    Event

---
+++ filmdesigners.at/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace filmdesigners.at.Models
+{
+    public class Event
+    {
+        public string EventID { get; set; }
+        public string Title { get; set; }
+        public string Picture { get; set; }
+        public string Teaser { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
