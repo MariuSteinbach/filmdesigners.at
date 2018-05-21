b5de2c0a6beec1277f240385a6575ae0b1c918a1
Marius Steinbach <m@WhackBook-Pro.local>
2018-05-21 13:09:34 +0200

    Added DSGVOAnswer

---
+++ filmdesigners.at/Models/DSGVOAnswer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace filmdesigners.at.Models
+{
+    public class DSGVOAnswer
+    {
+        public DSGVOAnswer()
+        {
+            public int ID
+		    {
+				get;
+				set;
+			}
+            public string Email
+    		{
+    			get;
+				set;
+    		}
+			public bool Accepted
+			{
+    			get;
+    			set;
+			}
+			public DateTime AcceptedAt
+			{
+    			get;
+    			set;
+			}
+			public string AcceptedFrom
+			{
+    			get;
+    			set;
+			}
+        }
+    }
+}
